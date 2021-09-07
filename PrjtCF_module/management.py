46f471120d944d3e216460fcd4ae41386cfbdcb6
KP_Hong <kphong16@daum.net>
2021-09-07 09:47:46 +0900

    edit management file

--- PrjtCF_module/management.py
+++ PrjtCF_module/management.py
@@ -49,7 +49,7 @@ class wtdrw_mngmnt:
     def wtdrw_exptd(self):
         """총 지출필요금액을 확인한 후 운영계좌 잔액을 초과하는 금액
         (추가 인출이 필요한 금액)을 계산하여 반환"""
-        cst_exptd = self.cstmng.ttl_exptd # 총 지출필요금액
+        cst_exptd = self.cstmng.cst_ttl # 총 지출필요금액
         oprtg_bal = self.oprtg.bal_end[self.idxno] # 운영계좌 잔액
         
         amt_rqrd = max(cst_exptd - oprtg_bal, 0)
@@ -77,4 +77,77 @@ class wtdrw_mngmnt:
             
             self.amt_wtdrw += tmp_wtdrw # 인출된 금액
             self.rsdl_wtdrw -= tmp_wtdrw # 인출 후 잔여 인출필요금액
+
+
+# Calculate cost amount
+class cst_mngmnt:
+    def __init__(self, idxno, cost, loan):
+        self.idxno = idxno
+        self.cost = cost
+        self.loan = loan
+        self.set_cst_oprtg()
+        self.set_cst_loan()
+        self.set_cst_ttl()
+        
+    def set_cst_oprtg(self):
+        # 해당 인덱스 기간 중 cost계좌 상 지출이 예정되어 있는 금액
+        ttlsum = 0
+        for key, val in self.cost.dct.items():
+            val_scdd = val.add_scdd[self.idxno]
+            setattr(self, key, val_scdd)
+            ttlsum += val_scdd
+        self.cst_oprtg = ttlsum
+        
+    def set_cst_loan(self):
+        # 해당 인덱스 기간 중 loan계좌 상 지출이 예정되어 있는 IR 및 fee
+        ttlfee = 0
+        ttlIR = 0
+        for key, val in self.loan.dct.items():
+            fee_scdd = val.fee.add_scdd[self.idxno]
+            setattr(self, "fee_"+key, fee_scdd)
+            ttlfee += fee_scdd
             
+            IR_scdd = -val.ntnl.bal_strt[self.idxno] * val.IR.rate
+            setattr(self, "IR_"+key, IR_scdd)
+            ttlIR += IR_scdd
+        self.cst_fnclfee = ttlfee
+        self.cst_fnclIR = ttlIR
+        self.cst_fncl = ttlfee + ttlIR
+        
+    def set_cst_ttl(self):
+        self.cst_ttl = self.cst_oprtg + self.cst_fncl
+        
+    def pay_oprtcst(self, oprtg):
+        """운영계좌에서 운영비용(cost) 지출"""
+        for key, acnt in self.cost.dct.items():
+            oprtg.send(self.idxno, getattr(self, key), acnt)
+            
+    def pay_fnclcst(self, oprtg):
+        """운영계좌에서 금융비용 지출"""
+        for key, acnt in self.loan.dct.items():
+            oprtg.send(self.idxno, getattr(self, "fee_"+key), acnt.fee)
+            oprtg.send(self.idxno, getattr(self, "IR_"+key), acnt.IR)
+            
+            
+# Calculate expected repayment of loan and repay loan.
+class repay_mngmnt:
+    """입력된 Loan instance에 대해서 
+    - 상환요구금액 계산
+    - 상환계좌로 자금 이체
+    - 상환계좌에서 loan 계좌로 상환 처리"""
+    def __init__(self, idxno, loan):
+        self.idxno = idxno
+        self.loan = loan
+        
+        # 상환요구금액 계산
+        self.exptd_rpy_cum = self.loan.ntnl.add_rsdl_cum[self.idxno]
+        self.ntnl_bal_end = -self.loan.ntnl.bal_end[self.idxno]
+        self.rpy_amt = min(self.exptd_rpy_cum, self.ntnl_bal_end)
+        
+    # Transfer repayment amount to repayment account
+    def trsf_rpy(self, oprtg, rpyacc):
+        oprtg.send(self.idxno, self.rpy_amt, rpyacc)
+    
+    # Repay loan from repayment account
+    def rpy_ntnl(self, rpyacc):
+        rpyacc.send(self.idxno, self.rpy_amt, self.loan.ntnl)
